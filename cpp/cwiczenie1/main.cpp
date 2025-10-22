2f05b5036cbb902758901aabf3a6bb0a099b40d2
borysb1 <borys.burzej@gmail.com>
2025-10-22 11:10:29 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	cout<<"podaj x: "<<endl;
+	int x;
+	cin>>x;
+	cout<<"podano: "<<x<<endl;
+	if(x>0){
+		cout<<"wieksze od 0 \n";
+	}else{
+		cout<<"mniejsze lub rowne \n";
+	}
+	if(x%5==0){
+		cout<<"podzielna przez piec :)\n";
+	}else{
+		cout<<"niepodzielna przez piec :(\n";
+	}
 	return 0;
 }
