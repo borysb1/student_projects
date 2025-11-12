53f5ed9b71edf9f5fde5be5f70fc89e737bbd814
borysb1 <borys.burzej@gmail.com>
2025-11-12 12:10:52 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -8,11 +8,21 @@ int main(){
 	for(int i=0;i<x;i++){
 		cout<<"*";
 	}*/
-	for(int i =0;i<5;i++){
+	/*for(int i =0;i<5;i++){
 		cout<<"\n";
 		for(int j = 0;j<5;j++){
 			cout<<"*";
 		}
+	}*/
+
+	cout<<"wpisz liczbe: "<<endl;
+	int liczba;
+	cin>>liczba;
+
+	if(liczba%liczba==0 &&  liczba%2!=0){
+		cout<<"Liczba pierwsza"<<endl;
+	}else{
+		cout<<"Nie liczba pierwsza"<<endl;
 	}
 	
 	return 0;
