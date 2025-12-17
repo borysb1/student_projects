3271359150a78cf817b1e4daedccc4ef1984ca8e
borysb1 <borys.burzej@gmail.com>
2025-12-17 12:08:53 +0000

    studentmachine send

--- cpp/tictactoe/main.cpp
+++ cpp/tictactoe/main.cpp
@@ -1,22 +1,72 @@
 #include <iostream>
-
+#include <ctime>
+#include <string>
+#include <cmath>
 using namespace std;
 
-int main(){
-	char tab1[3][3] = {
-		{'W','W','W'},
-		{'W','W','W'},
-		{'W','W','W'}
-	};
-	int wiersze = 3;
-	int kolumny = 3;
-	for(int i = 0;i<wiersze;i++){
-		for(int j=0;j<kolumny;j++){
-			cout<<tab1[i][j];
+void wypisz(char tab[3][3]){
+	for(int i=0; i<3; i++){
+		for(int j=0;j<3;j++){
+			cout<<tab[i][j]<<" ";
 		}
-		cout<<endl;
+	cout<<endl;
 	}
+}
 
+int main(){
 	
+	char tab[3][3]={
+		{'-','-','-'},
+		{'-','-','-'},
+		{'-','-','-'}
+	};
+
+	wypisz(tab);
+
+	for(int a=0;a<=9;a++){
+		
+	ruch1:
+		cout<<"Gdzie kolo?"<<endl;
+		int a1,a2;
+		cin>>a1;
+		cin>>a2;
+
+		if(tab[a1][a2]!='-'){
+			cout<<"Miejsce zajete, wybierz inne"<<endl;
+			a--;
+			goto ruch1;
+		}else{
+			tab[a1][a2]='O';
+		}
+		wypisz(tab);
+
+	ruch2:
+		cout<<"Gdzie krzyzyk?"<<endl;
+		int b1,b2;
+		cin>>b1;
+		cin>>b2;
+
+		if(tab[b1][b2]!='-'){
+			cout<<"Miejsce zajete, wybierz inne"<<endl;
+			a--;
+			goto ruch2;
+		}else{
+			tab[b1][b2]='X';
+		}
+
+		wypisz(tab);
+
+
+
+	if(tab[0][0]=='O' && tab[0][1]=='O' && tab[0][2]=='O'){
+		cout<<"Wygrywa kolo ez?"<<endl;
+	}else if(tab[1][0]=='O' && tab[1][1]=='O' && tab[1][2]=='O'){
+		cout<<"Wygrywa kolo ez?"<<endl;
+	}else if(tab[2][0]=='O' && tab[2][1]=='O'&& tab[2][2]=='O'){
+		cout<<"Wygrywa kolo ez?"<<endl;
+	}else if(tab[0][0]=='O' && tab[0][1]=='O' && tab[0][2]=='O'){
+		cout<<"Wygrywa kolo ez?"<<endl;
+	}
+	}	
 	return 0;
 }
